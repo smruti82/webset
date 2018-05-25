f5d823de4205fac06146ab0c7776701984330813
smruti82 <smrutiswain82@gmail.com>
2018-05-25 10:54:16 +0530

    api id

--- server/monserver.js
+++ server/monserver.js
@@ -1,6 +1,8 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 
+var{ObjectID}=require('mongodb');
+
 var {mongoose}= require('./db/mongoose');
 var {User}=require('./models/user');
 
@@ -30,6 +32,25 @@ res.send(users);
 res.status(400).send(e);
 });
 
+});
+
+app.get('/user/:id',(req,res)=>{
+
+var id=req.params.id;
+
+if(!ObjectID.isValid(id)){
+return res.status(404).send();
+}
+
+User.findById(id).then((user)=>{
+if(!user){
+return res.status(404).send();
+}
+res.send({user});
+
+}).catch((e)=>{
+	res.status(400).send();
+});
 });
 
 
